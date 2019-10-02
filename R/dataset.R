3516e3d113701bd6bed76297a52721e83936a5b0
Michael DeWitt <me.dewitt.jr@gmail.com>
2019-10-02 06:56:48 -0400

    added alcohol consumption from WHO and econ data from WB in order to generate another example

--- R/dataset.R
+++ R/dataset.R
@@ -12,3 +12,21 @@
 #' }
 
 "synth.data"
+
+#' Alcohol Data
+#' @format a data.frame with 5107 rows and 8 columns:
+#' \describe{
+#'   \item{country_name}{The name of the country}
+#'   \item{year}{year}
+#'   \item{consumption}{name of the experimental unit}
+#'   \item{country_code}{outcome of interest}
+#'   \item{labor_force_participation_rate}{Covariate 1}
+#'   \item{mobile_cellular_subscriptions}{Covariate 2}
+#'   \item{inflation}{Covariate 3}
+#'   \item{manufacturing}{Covariate 3}
+#'   \item{country_num}{Covariate 3}
+#'    ...
+#' }
+#' @source World Health Organization, World Bank
+
+"alcohol"
