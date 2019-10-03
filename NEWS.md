0e1087cd5507765941df721a01e5dfe13e4d2206
Michael DeWitt <me.dewitt.jr@gmail.com>
2019-10-03 09:08:08 -0400

    refresh website and added bugs

--- NEWS.md
+++ NEWS.md
@@ -1,4 +1,4 @@
-# SCtools 0.2.9000
+# SCtools 0.3.0
 
 * Added a `NEWS.md` file to track changes to the package.
 * `plot.placebos` has changed to `plot_placebos` to avoid S3 conflicts
--- R/multiple.synth.R
+++ R/multiple.synth.R
@@ -52,6 +52,38 @@
 #' @return Data frame. Each column contains the outcome values for every 
 #' time-point for one unit or its synthetic control. The last column contains 
 #' the time-points.
+#' @examples \dontrun{
+#' library(Synth)
+#' data(synth.data)
+#' set.seed(42)
+#'
+#' multi <- multiple.synth(foo = synth.data,
+#'                        predictors = c("X1", "X2", "X3"),
+#'                        predictors.op = "mean",
+#'                        dependent = "Y",
+#'                        unit.variable = "unit.num",
+#'                        time.variable = "year",
+#'                        treatment.time = 1983,
+#'                        special.predictors = list(
+#'                          list("Y", 1991, "mean"),
+#'                          list("Y", 1985, "mean"),
+#'                          list("Y", 1980, "mean")
+#'                        ),
+#'                        treated.units = 7,
+#'                        control.units = c(29, 2, 13, 17, 32, 38),
+#'                        time.predictors.prior = c(1984:1989),
+#'                        time.optimize.ssr = c(1984:1990),
+#'                        unit.names.variable = "name",
+#'                        time.plot = 1984:1996, gen.placebos = TRUE, Sigf.ipop = 3 )
+#' 
+#' multi
+#' 
+#' plac_dist(multi)
+#' 
+#' 
+#' }
+#' 
+#' 
 #'@export
 
 
